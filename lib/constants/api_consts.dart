aaf54661ccef240d8b123e95d1249854f01d8a8e
Vinícius Bruno <vinnibruno20@gmail.com>
2023-03-25 19:35:55 -0300

    Changes: add Buttons Like and Dislike

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,3 +1,3 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
 String REPO_URL = "https://github.com/Vinnybrunn00/App_ChatGPT";
